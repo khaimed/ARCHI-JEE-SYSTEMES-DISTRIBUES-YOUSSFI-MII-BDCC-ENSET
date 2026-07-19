7505a127b719c6009893c6094f8f6689874cc8d8
khaimed <46203094+khaimed@users.noreply.github.com>
2026-07-19 14:22:36 +0100

    upload final projet

---
+++ Projet Spring Angular JWT Spring Security/ebanking-backend/src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -0,0 +1,58 @@
+package ma.enset.ebankingbackend;
+
+import ma.enset.ebankingbackend.entities.CurrentAccount;
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.entities.SavingAccount;
+import ma.enset.ebankingbackend.enums.AccountStatus;
+import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
+import ma.enset.ebankingbackend.repositories.BankAccountRepository;
+import ma.enset.ebankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class EbankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository){
+        return args -> {
+            Stream.of("Hassan", "Yassine", "Aicha").forEach(name ->{
+                Customer customer=new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@email.com");
+                customerRepository.save(customer);
+            });
+            customerRepository.findAll().forEach(cust ->{
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*9000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*9000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+            });
+        };
+    }
+}
